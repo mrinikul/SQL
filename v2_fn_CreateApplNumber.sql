7f8f561fa11acb2211f057a528f95c8f69ad6681
mrinikul <mrinikul@gmail.com>
2018-09-24 17:10:16 -0700

    first commit

---
+++ v2_fn_CreateApplNumber.sql
@@ -0,0 +1,59 @@
+﻿
+-- =============================================
+-- Author:		Irina Eva
+-- Create date: 12/07/2017
+-- Description:	<Description, ,>
+-- =============================================
+CREATE FUNCTION [dbo].[v2_fn_CreateApplNumber]
+(
+	-- Add the parameters for the function here
+
+)
+RETURNS VARCHAR(7)
+AS
+BEGIN
+	-- Declare the return variable here
+	DECLARE @newApplNumb VARCHAR(7) = ''
+
+	-- Add the T-SQL statements to compute the return value here
+	DECLARE @minAppl VARCHAR(7) = '2000000'
+	DECLARE @maxAppl VARCHAR(7) = '2999999' 
+	
+
+	DECLARE @minApplFound VARCHAR(7) = (SELECT MIN(ApplNumb) FROM Workforce WHERE CONVERT(INT,ApplNumb) BETWEEN CONVERT(INT,@minAppl) AND CONVERT(INT,@maxAppl))
+	IF @minApplFound <> @minAppl
+		SET @maxAppl = @minApplFound
+
+	DECLARE @rowNumber BIGINT =
+		(
+		SELECT TOP 1 rowNumber FROM 
+			(
+			SELECT ROW_NUMBER() OVER (ORDER BY ApplNumb)+CONVERT(INT,@minAppl)-1 rowNumber, ApplNumb
+			FROM Workforce 
+			WHERE CONVERT(INT,ApplNumb) BETWEEN CONVERT(INT,@minAppl) AND CONVERT(INT,@maxAppl)
+			) t
+		WHERE rowNumber<>CONVERT(BIGINT,ApplNumb)
+		ORDER BY rowNumber,ApplNumb
+		)
+
+	IF @rowNumber IS NULL
+		IF NOT EXISTS(SELECT 1 FROM Workforce WHERE ApplNumb=@minAppl)
+			SET @newApplNumb = @minAppl
+		ELSE IF NOT EXISTS(SELECT 1 FROM Workforce WHERE ApplNumb=@maxAppl)
+			SET @newApplNumb = @maxAppl
+		ELSE
+			SET @newApplNumb = ''
+	ELSE
+		SET @newApplNumb = CONVERT(VARCHAR(7), @rowNumber)
+		
+	--IF @newApplNumb = ''
+	--	RAISERROR ('ERROR from v2_fn_CreateApplNumb - Cant create new ApplNumb',-1,-1 ); 
+		
+	--SELECT @newApplNumb newApplNumb
+	--SELECT * FROM WorkForce where ApplNumb=@newApplNumb
+
+	-- Return the result of the function
+	RETURN @newApplNumb
+
+END
+
---
+++ v2_fn_CreateErNumber.sql
@@ -0,0 +1,44 @@
+﻿
+-- =============================================
+-- Author:		Irina Eva
+-- Create date: 12/07/2017
+-- Description:	<Description, ,>
+-- =============================================
+CREATE FUNCTION [dbo].[v2_fn_CreateErNumber] ()
+RETURNS INT
+AS
+BEGIN
+	-- Declare the return variable here
+	DECLARE @er_num INT
+
+	-- Add the T-SQL statements to compute the return value here
+	
+	DECLARE @minEr_num INT = 1
+	DECLARE @maxEr_num INT = ISNULL((SELECT MAX(er_num) FROM er),1) + 1
+	DECLARE @minEr_numFound INT = ISNULL((SELECT MIN(er_num) FROM er),1)
+	IF @minEr_num < @minEr_numFound
+		SET @maxEr_num = @minEr_numFound
+
+	SET @er_num =
+		(
+		SELECT TOP 1 rowNumber FROM 
+			(
+			SELECT ROW_NUMBER() OVER (ORDER BY er_num) + @minEr_num - 1 rowNumber, er_num
+			FROM er WHERE er_num BETWEEN @minEr_num AND @maxEr_num
+			) t
+		WHERE rowNumber<>CONVERT(BIGINT,er_num)
+		ORDER BY rowNumber,er_num
+		)
+
+	IF @er_num IS NULL
+		IF NOT EXISTS(SELECT TOP 1 1 FROM er WHERE er_num = @minEr_num)
+			SET @er_num = @minEr_num
+		ELSE
+			SET @er_num = @maxEr_num
+	
+
+	-- Return the result of the function
+	RETURN @er_num
+
+END
+
